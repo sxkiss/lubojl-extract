36799cd9aaa025a6b8b377355010f843710e15a3
sxkiss <admin@sxkiss.com>
2026-08-07 23:03:00 +0800

    Add RecordSetVideoActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/RecordSetVideoActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecordSetVideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_set_video);
+        // TODO: Implement MediaProjection + Camera2 for screen/video recording
+    }
+}
