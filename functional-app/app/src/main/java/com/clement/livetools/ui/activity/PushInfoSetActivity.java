286a20c361fdd50503cd3bd6fcf00cef5a5f6608
sxkiss <admin@sxkiss.com>
2026-08-07 23:17:35 +0800

    Add PushInfoSetActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/PushInfoSetActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PushInfoSetActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_info_set);
+        // TODO: Implement push notification settings from original APK
+    }
+}
