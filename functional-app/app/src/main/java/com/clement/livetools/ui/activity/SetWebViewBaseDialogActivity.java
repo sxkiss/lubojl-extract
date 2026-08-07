f0a9465210abfa8403a01b6059ef79b3dceeac5a
sxkiss <admin@sxkiss.com>
2026-08-07 23:19:03 +0800

    Add SetWebViewBaseDialogActivity and CaptureActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/SetWebViewBaseDialogActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetWebViewBaseDialogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_webview_base_dialog);
+        // TODO: Implement BaseWebView functionality
+    }
+}
