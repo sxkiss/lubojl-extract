353dbb5d8a0dacdb7f916ae348e129ed66e288f9
sxkiss <admin@sxkiss.com>
2026-08-07 23:02:33 +0800

    Add AudioRecordActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/AudioRecordActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioRecordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_record);
+        // TODO: Implement LAME MP3 encoder for audio recording
+    }
+}
