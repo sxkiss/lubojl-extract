3334091389844279a5e435cc5c1310c9ed051e8a
sxkiss <admin@sxkiss.com>
2026-08-07 23:03:33 +0800

    Add RecordSetAudioActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/RecordSetAudioActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecordSetAudioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_set_audio);
+        // TODO: Implement screen recording with audio
+    }
+}
