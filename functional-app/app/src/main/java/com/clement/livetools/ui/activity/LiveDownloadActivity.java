63c6550e5b46352f71d7ca2d203b5704f7469664
sxkiss <admin@sxkiss.com>
2026-08-07 22:32:17 +0800

    Add LiveDownloadActivity with basic UI

---
+++ functional-app/app/src/main/java/com/clement/livetools/ui/activity/LiveDownloadActivity.java
@@ -0,0 +1,13 @@
+package com.clement.livetools.ui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LiveDownloadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_download);
+        // TODO: Implement live download functionality from original APK
+    }
+}
